00e1d52ebdb03c88ee3efe0b617389d5b1877cce
aslanali <aslanali@users.noreply.github.com>
2023-10-08 17:59:38 +0530

    Add files via upload
    added test_calculator with unit tests

---
+++ test_calculator.py
@@ -0,0 +1,31 @@
+import pytest
+from calculator import add, subtract, multiply, divide
+
+def test_add():
+  """Test the add function."""
+  assert add(1, 2) == 3
+  assert add(5, 10) == 15
+
+def test_subtract():
+  """Test the subtract function."""
+  assert subtract(10, 5) == 5
+  assert subtract(15, 20) == -5
+
+def test_multiply():
+  """Test the multiply function."""
+  assert multiply(2, 2) == 4
+  assert multiply(5, 10) == 50
+
+def test_divide():
+  """Test the divide function."""
+  assert divide(10, 5) == 2
+  assert divide(15, 20) == 0.75
+
+def test_add_negative_numbers():
+  """Test that the add function can correctly calculate the sum of two negative numbers."""
+  assert add(-1, -2) == -3
+
+def test_divide_by_zero():
+  """Test that the divide function can correctly handle dividing by zero."""
+  with pytest.raises(ZeroDivisionError):
+    divide(10, 0)
